625875bdf0a3075915a1753ef0727eac90933932
ACT-STUDENT <ACT-STUDENT@LAB1-PC11>
2025-09-02 09:32:50 +0800

    Exer#6

---
+++ Dumoran_Simple_Inheritance/Dumoran_Simple_Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumoran_Simple_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name:Dumoran,Rey C.
+            //Section:BSIT-301
+            //Exer: Simple Inheritance
+
+            
+            RegularStudent regularStudent = new RegularStudent();
+            //Set the Student name
+            regularStudent.Name = "Rey";
+            //Set the program to student to enrolled
+            regularStudent.Program = "BSIT";
+            //set the section to student to belongs to
+            regularStudent.Section = "IT-301";
+            //Display basic information to regular student
+            regularStudent.BasicInfo();
+            regularStudent.SectionEnrolled();
+
+            IrregularStudent irregularStudent = new IrregularStudent();
+            //Set the Student name
+            irregularStudent.Name = "John";
+            //set the section to student to belongs to
+            irregularStudent.Program = "BSHM";
+            //set the units if the student is irregular
+            irregularStudent.UnitsEnrolled = 18;
+            //Display basic information to regular student
+            irregularStudent.BasicInfo();
+            irregularStudent.EnrolledSemUnits();
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Dumoran_Simple_Inheritance/Dumoran_Simple_Inheritance/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumoran_Simple_Inheritance
+{
+    internal class Student
+    {
+        //property student name
+        public string Name { get; set; }
+        //property to store the student program 
+        public string Program { get; set; }
+        
+        //method to display basic information to student
+        public void BasicInfo() 
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Program: " + Program);
+        }
+      
+    }
+    //derived class of regular student
+    class RegularStudent : Student 
+    {//property display the section of student
+        public string Section { get; set; }
+
+        //method
+        public void SectionEnrolled() 
+        {
+            Console.WriteLine("Section: " + Section);
+            Console.WriteLine();
+        }
+    }
+    //derived class of irregular student
+    class IrregularStudent : Student
+    {
+        //property to display if what units enrolled
+        public int UnitsEnrolled { get; set; }
+
+        //method
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine("UnitsEnrolled: " + UnitsEnrolled);
+            Console.WriteLine();
+        }
+
+
+    }
+    
+
+}
